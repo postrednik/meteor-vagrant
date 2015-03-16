c6f8b8db9a733ec3173b0d0979cba3edf01c3121
Roman Piták <roman@pitak.net>
2015-03-16 11:09:41 +0100

    replaced echo by printf

--- install.sh
+++ install.sh
@@ -14,18 +14,18 @@ INSTALL_URL="https://raw.githubusercontent.com/postrednik/meteor-vagrant/master"
 VAGRANTFILE_URL="$INSTALL_URL/Vagrantfile"
 VAGRANTDIR_URL="$INSTALL_URL/vagrant"
 
-echo "downloading package files"
+printf 'downloading package files\n'
 
 curl "$VAGRANTFILE_URL" -o "Vagrantfile"
 mkdir -p vagrant/
 curl "$VAGRANTDIR_URL/{config.yaml,meteor.rb,provision.sh,init-meteor.sh}" -o "vagrant/#1"
 
-echo "adding records to .gitignore"
+printf 'adding records to .gitignore\n'
 cat >> .gitignore <<GITIGNORE
 .vagrant/
 vagrant/
 Vagrantfile
 GITIGNORE
 
-echo "installation completed"
-echo "now you can run: vagrant up"
+printf 'installation completed\n'
+printf 'now you can run: vagrant up\n'
